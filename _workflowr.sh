db6f6d1310706e06f8954391f7388fa4b4f18c14
James Ashmore <jma1991@icloud.com>
2020-10-14 10:51:49 +0100

    n/a

---
+++ _workflowr.sh
@@ -0,0 +1,9 @@
+git add _workflowr.R _workflowr.sh _workflowr.yml
+
+git add analysis/_site.yml analysis/about.Rmd analysis/index.Rmd analysis/license.Rmd analysis/01-quality-control.Rmd analysis/02-normalization.Rmd analysis/03-feature-selection.Rmd analysis/04-reduced-dimensions.Rmd analysis/05-clustering.Rmd analysis/06-doublet-detection.Rmd analysis/07-cell-cycle.Rmd analysis/08-marker-detection.Rmd analysis/09-cell-annotation.Rmd
+
+git commit -m "publish"
+
+git add docs/about.html docs/index.html docs/license.html docs/01-quality-control.html docs/02-normalization.html docs/03-feature-selection.html docs/04-reduced-dimensions.html docs/05-clustering.html docs/06-doublet-detection.html docs/07-cell-cycle.html docs/08-marker-detection.html docs/09-cell-annotation.html
+
+git push origin master
