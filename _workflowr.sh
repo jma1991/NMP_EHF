b3f8573c77d12ba136e7a78f5da618fade8e60a0
James Ashmore <jma1991@icloud.com>
2020-12-09 13:50:58 +0000

    publish

--- _workflowr.sh
+++ _workflowr.sh
@@ -1,10 +1,12 @@
+git add .gitignore
+
 git add _workflowr.R _workflowr.sh _workflowr.yml
 
 git add analysis/_site.yml analysis/about.Rmd analysis/index.Rmd analysis/license.Rmd analysis/01-quality-control.Rmd analysis/02-normalization.Rmd analysis/03-feature-selection.Rmd analysis/04-reduced-dimensions.Rmd analysis/05-clustering.Rmd analysis/06-doublet-detection.Rmd analysis/07-cell-cycle.Rmd analysis/08-marker-detection.Rmd analysis/09-cell-annotation.Rmd analysis/10-human-integration.Rmd
 
 git add docs/about.html docs/index.html docs/license.html docs/01-quality-control.html docs/02-normalization.html docs/03-feature-selection.html docs/04-reduced-dimensions.html docs/05-clustering.html docs/06-doublet-detection.html docs/07-cell-cycle.html docs/08-marker-detection.html docs/09-cell-annotation.html docs/10-human-integration.html
 
-git add docs/figure/01-quality-control.Rmd docs/figure/02-normalization.Rmd docs/figure/03-feature-selection.Rmd docs/figure/04-reduced-dimensions.Rmd docs/figure/05-clustering.Rmd docs/figure/06-doublet-detection.Rmd docs/figure/07-cell-cycle.Rmd docs/figure/08-marker-detection.Rmd docs/figure/09-cell-annotation.Rmd docs/figure/10-human-integration.Rmd
+git add docs/site_libs docs/figure/01-quality-control.Rmd docs/figure/02-normalization.Rmd docs/figure/03-feature-selection.Rmd docs/figure/04-reduced-dimensions.Rmd docs/figure/05-clustering.Rmd docs/figure/06-doublet-detection.Rmd docs/figure/07-cell-cycle.Rmd docs/figure/08-marker-detection.Rmd docs/figure/09-cell-annotation.Rmd docs/figure/10-human-integration.Rmd
 
 git commit -m "publish"
 
