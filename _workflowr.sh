87cba3a2d74a5cd0ae6f6535aab3b64fcf29b523
James Ashmore <jma1991@icloud.com>
2020-12-09 13:41:45 +0000

    publish

--- _workflowr.sh
+++ _workflowr.sh
@@ -4,6 +4,8 @@ git add analysis/_site.yml analysis/about.Rmd analysis/index.Rmd analysis/licens
 
 git commit -m "publish"
 
-git add docs/about.html docs/index.html docs/license.html docs/01-quality-control.html docs/02-normalization.html docs/03-feature-selection.html docs/04-reduced-dimensions.html docs/05-clustering.html docs/06-doublet-detection.html docs/07-cell-cycle.html docs/08-marker-detection.html docs/09-cell-annotation.html docs/10-human-integration.html
+git add docs/docs/01-quality-control.html docs/02-normalization.html docs/03-feature-selection.html docs/04-reduced-dimensions.html docs/05-clustering.html docs/06-doublet-detection.html docs/07-cell-cycle.html docs/08-marker-detection.html docs/09-cell-annotation.html docs/10-human-integration.html
+
+git add docs/figure/01-quality-control.Rmd docs/figure/02-normalization.Rmd docs/figure/03-feature-selection.Rmd docs/figure/04-reduced-dimensions.Rmd docs/figure/05-clustering.Rmd docs/figure/06-doublet-detection.Rmd docs/figure/07-cell-cycle.Rmd docs/figure/08-marker-detection.Rmd docs/figure/09-cell-annotation.Rmd docs/figure/10-human-integration.Rmd
 
 git push origin master
